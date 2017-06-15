7811c602c48d2db5a96babd23b1db06663ff01a2
Louis Vallance <louisvallance@hotmail.co.uk>
2017-06-15 13:53:57 +0200

    Bug fix
    - Fixed a bug where a data check analysis would crash if any items were
    removed during group validation checks

--- Application_Files/code/main/cleanup.m
+++ Application_Files/code/main/cleanup.m
@@ -6,7 +6,7 @@ function [] = cleanup(status)
 %   is not required to run this file.
 %   
 %   Quick Fatigue Tool 6.11-00 Copyright Louis Vallance 2017
-%   Last modified 14-Jun-2017 14:41:28 GMT
+%   Last modified 15-Jun-2017 12:32:06 GMT
     
     %%
     
@@ -72,7 +72,7 @@ if status == 1.0
     % Write file header
     fprintf(fid, 'Quick Fatigue Tool 6.11-00\r\n');
     fprintf(fid, '(Copyright Louis Vallance 2017)\r\n');
-    fprintf(fid, 'Last modified 14-Jun-2017 14:41:28 GMT\r\n\r\n');
+    fprintf(fid, 'Last modified 15-Jun-2017 12:32:06 GMT\r\n\r\n');
     
     % Continue writing the file
     fprintf(fid, 'THE ANALYSIS WAS ABORTED FOR THE FOLLOWING REASON(S):');
@@ -808,7 +808,7 @@ if status == 1.0
         rmappdata(0, 'E080')
     end
     if getappdata(0, 'E081') == 1.0
-        fprintf(fid, '\r\n\r\n***ERROR: FRF envelopes are defined as a cell with the frfEnvelope variable, but the number of definitions does not match the number of groups');
+        fprintf(fid, '\r\n\r\n***ERROR: FRF envelopes are defined as a cell with the FATIGUE_RESERVE_FACTOR option, but the number of definitions does not match the number of groups');
         fprintf(fid, '\r\n-> There are %.0f FRF envelope definitions and %.0f analysis groups', getappdata(0, 'error_log_081_NfrfDefinitions'), getappdata(0, 'error_log_081_NGroups'));
         fprintf(fid, '\r\n-> Make sure the FRF envelope definition references the correct number of analysis groups');
         fprintf(fid, '\r\n\r\nError code: E081');
@@ -816,8 +816,8 @@ if status == 1.0
     end
     if getappdata(0, 'E082') == 1.0
         fprintf(fid, '\r\n\r\n***ERROR: The FRF envelope definition is invalid');
-        fprintf(fid, '\r\n-> For a single analysis group, frfEnvelope should be a single value');
-        fprintf(fid, '\r\n-> For multiple analysis groups, frfEnvelope must either be a numerical array of envelope numbers, or a cell array of envelope numbers and/or user defined FRF files');
+        fprintf(fid, '\r\n-> For a single analysis group, FATIGUE_RESERVE_FACTOR should be a single value');
+        fprintf(fid, '\r\n-> For multiple analysis groups, FATIGUE_RESERVE_FACTOR must either be a numerical array of envelope numbers, or a cell array of envelope numbers and/or user defined FRF files');
         fprintf(fid, '\r\n\r\nError code: E082');
         rmappdata(0, 'E082')
     end
@@ -1094,7 +1094,7 @@ if status == 1.0
         rmappdata(0, 'E119')
     end
     if getappdata(0, 'E120') == 1.0
-        fprintf(fid, '\r\n\r\n***ERROR: FRF mean stress (tensile) normalization parameters are defined as a cell with the frfNormParamMeanT variable, but the number of definitions does not match the number of user-defined FRF envelopes');
+        fprintf(fid, '\r\n\r\n***ERROR: FRF mean stress (tensile) normalization parameters are defined as a cell with the frfNormParamMeanT environment variable, but the number of definitions does not match the number of user-defined FRF envelopes');
         fprintf(fid, '\r\n-> There are %.0f FRF mean stress (tensile) normalization parameters and %.0f user-defined FRF envelopes', getappdata(0, 'error_log_120_NfrfDefinitions'), getappdata(0, 'error_log_120_NGroups'));
         fprintf(fid, '\r\n-> Make sure the FRF mean stress (tensile) normalization parameter definition references the correct number of user-defined FRF envelopes');
         fprintf(fid, '\r\n\r\nError code: E120');
@@ -1122,7 +1122,7 @@ if status == 1.0
         rmappdata(0, 'E123')
     end
     if getappdata(0, 'E124') == 1.0
-        fprintf(fid, '\r\n\r\n***ERROR: FRF stress amplitude normalization parameters are defined as a cell with the frfNormParamAmp variable, but the number of definitions does not match the number of user-defined FRF envelopes');
+        fprintf(fid, '\r\n\r\n***ERROR: FRF stress amplitude normalization parameters are defined as a cell with the frfNormParamAmp environment variable, but the number of definitions does not match the number of user-defined FRF envelopes');
         fprintf(fid, '\r\n-> There are %.0f FRF stress amplitude normalization parameters and %.0f user-defined FRF envelopes', getappdata(0, 'error_log_124_NfrfDefinitions'), getappdata(0, 'error_log_124_NGroups'));
         fprintf(fid, '\r\n-> Make sure the FRF stress amplitude normalization parameter definition references the correct number of user-defined FRF envelopes');
         fprintf(fid, '\r\n\r\nError code: E124');
@@ -1143,7 +1143,7 @@ if status == 1.0
         rmappdata(0, 'E126')
     end
     if getappdata(0, 'E127') == 1.0
-        fprintf(fid, '\r\n\r\n***ERROR: FRF mean stress (compressive) normalization parameters are defined as a cell with the frfNormParamMeanC variable, but the number of definitions does not match the number of user-defined FRF envelopes');
+        fprintf(fid, '\r\n\r\n***ERROR: FRF mean stress (compressive) normalization parameters are defined as a cell with the frfNormParamMeanC environment variable, but the number of definitions does not match the number of user-defined FRF envelopes');
         fprintf(fid, '\r\n-> There are %.0f FRF mean stress (compressive) normalization parameters and %.0f user-defined FRF envelopes', getappdata(0, 'error_log_127_NfrfDefinitions'), getappdata(0, 'error_log_127_NGroups'));
         fprintf(fid, '\r\n-> Make sure the FRF mean stress (compressive) normalization parameter definition references the correct number of user-defined FRF envelopes');
         fprintf(fid, '\r\n\r\nError code: E127');
