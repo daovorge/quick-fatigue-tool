c5ad5acd708dc68911d975592abf66b5223fda62
fsgeek91 <louisvallance@hotmail.co.uk>
2018-03-07 13:21:08 +0100

    Minor update

--- Application_Files/code/material_manager/createMaterialDirect.m
+++ Application_Files/code/material_manager/createMaterialDirect.m
@@ -8,10 +8,117 @@ function [] = createMaterialDirect()
 %   Quick Fatigue Tool 6.11-13 Copyright Louis Vallance 2018
 %   Last modified 07-Mar-2018 07:50:12 GMT
 
+%%
+
+%% Variable name look-up table
+% <GENERAL PROPERTIES>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% default_algorithm  | Default analysis algorithm
+% default_msc        | Default mean stress correction
+% class              | Material classification
+% behavior           | Material behaviour
+% reg_model          | Regression model
+% cael               | Constant amplitude endurance limit (2Nf)
+% cael_active        | 
+% ndCompression      | No damage for fully compressive cycles
+% comment            | User comment
+% ___________________|_____________________________________________________
+% <MECHANICAL PROPERTIES>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% e                  | Young's Modulus
+% e_active           | 
+% uts                | Ultimate tensile strengthr
+% ucs                | Ultimate compressive strength
+% uts_active         | 
+% proof              | 0.2% strain proof stress
+% proof_active       | 
+% poisson            | Poisson's ratio
+% poisson_active     | 
+% ___________________|_____________________________________________________
+% <FATIGUE PROPERTIES>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% s_values           | S-N data S-values
+% n_values           | S-N data N-values
+% r_values           | S-N data R-values
+% sf                 | Fatigue strength coefficient
+% sf_active          | 
+% b                  | Fatigue strength exponent
+% b_active           | 
+% b2                 | Second fatigue strength exponent
+% b2Nf               | Life above which to use b2
+% ef                 | Fatigue ductility coefficient
+% ef_active          | 
+% c                  | Fatigue ductility exponent
+% c_active           | 
+% kp                 | Cyclic strain-hardening coefficient
+% kp_active          | 
+% np                 | Cyclic strain-hardening exponent
+% np_active          | 
+% nssc               | Normal stress sensitivity constant
+% nssc_active        | 
+% ___________________|_____________________________________________________
+% <STRESS-BASED FAILURE CRITERIA>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% failStress_tsfd    | Tensile stress, 11-direction
+% failStress_csfd    | Compressive stress, 11-direction
+% failStress_tstd    | Tensile stress, 22-direction
+% failStress_cstd    | Compressive stress, 22-direction
+% failStress_tsttd   | Tensile stress, 33-direction
+% failStress_csttd   | Compressive stress, 33-direction
+% failStress_shear   | Shear strength, 12-plane
+% failStress_cross12 | Cross-product coefficient, 12-plane
+% failStress_cross23 | Cross-product coefficient, 23-plane
+% failStress_limit12 | Stress limit, 12-plane
+% failStress_limit23 | Stress limit, 23-plane
+% ___________________|_____________________________________________________
+% <MAXIMUM STRAIN FAILURE THEORY>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% failStrain_tsfd    | Tensile strain, fibre direction
+% failStrain_csfd    | Compressive strain, fibre direction
+% failStrain_tstd    | Tensile strain, transverse direction
+% failStrain_cstd    | Compressive strain, transverse direction
+% failStrain_shear   | Shear strain
+% failStrain_e11     | Elastic modulus, fibre direction
+% failStrain_e22     | Elastic modulus, transverse direction
+% failStrain_g12     | Shear modulus, 12-plane
+% ___________________|_____________________________________________________
+% <HASHIN'S DAMAGE INITIATION CRITERIA>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% hashin_alpha       | Shear influence parameter
+% hashin_lts         | Longitudinal tensile strength
+% hashin_lcs         | Longitudinal compressive strength
+% hashin_tts         | Transverse tensile strength
+% hashin_tcs         | Transverse compressive strength
+% hashin_lss         | Longutudinal shear strength
+% hashin_tss         | Transverse shear strength
+% ___________________|_____________________________________________________
+% <LARC05 DAMAGE INITIATION CRITERIA>
+% Variable           | Meaning
+% ___________________|_____________________________________________________
+% larc05_lts         | Longitudinal tensile strength
+% larc05_lcs         | Longitudinal compressive strength
+% larc05_tts         | Transverse tensile strength
+% larc05_tcs         | Transverse compressive strength
+% larc05_lss         | Longitudinal shear strength
+% larc05_tss         | Transverse shear strength
+% larc05_shear       | Shear modulus
+% larc05_nl          | Longitudinal slope coefficient
+% larc05_nt          | Transverse slope coefficient
+% larc05_alpha0      | Fracture plane angle for pure compression
+% larc05_phi0        | Initial fibre misalignment angle
+% larc05_iterate     | Allow iterative solution for larc05_phi0
+% ___________________|_____________________________________________________
+
 %% Material name
 materialName = 'Steel';
 
-%% Add material data
+%% Material data
 material_properties = struct(...
 'default_algorithm', 7.0,...
 'default_msc', 4.0,...
